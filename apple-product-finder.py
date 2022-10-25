9578c1802a5dd5ff8dae329cba2308331f533ffc
wes1993 <52070589+wes1993@users.noreply.github.com>
2022-10-25 15:19:44 +0200

    Added Errors Handlers

--- apple-product-finder.py
+++ apple-product-finder.py
@@ -1,14 +1,22 @@
 import requests
 import sys
+try:
 
-model = input("Enter iPhone Model all without space (Ex. iphone13pro) : ")
+    model = input("Enter iPhone Model all without space (Ex. iphone13pro) : ")
+    
+    sys.stdout.flush()
+    url = "https://www.apple.com/it/shop/product-locator-meta?family=" + model
+    #Do The Request to Apple Servers
+    r = requests.get(url)
+    # Get Number of near store
+    sn = len(r.json()['body']['productLocatorOverlayData']['productLocatorMeta']['products'])
+    for j in range (sn):
+      print (r.json()['body']['productLocatorOverlayData']['productLocatorMeta']['products'][j]['productTitle'] + " - " + r.json()['body']['productLocatorOverlayData']['productLocatorMeta']['products'][j]['partNumber'])
 
-sys.stdout.flush()
-url = "https://www.apple.com/it/shop/product-locator-meta?family=" + model
-#Do The Request to Apple Servers
-r = requests.get(url)
-# Get Number of near store
-sn = len(r.json()['body']['productLocatorOverlayData']['productLocatorMeta']['products'])
-for j in range (sn):
-  print (r.json()['body']['productLocatorOverlayData']['productLocatorMeta']['products'][j]['productTitle'] + " - " + r.json()['body']['productLocatorOverlayData']['productLocatorMeta']['products'][j]['partNumber'])
+except KeyError as ke:
+    print('########Errore!!! Prodotto Inserito non corretto!!! Verifica il modello == ', model)
+    print(ke)
+    print(r.json())
 
+except KeyboardInterrupt:
+    print('\n Programma Terminato Correttamente')
